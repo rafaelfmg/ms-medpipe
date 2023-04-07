79a45e71d73b3e7f044680d7f19b4f924d73b47a
rafaelfmg <rafaelf.fmg@gmail.com>
2023-04-07 16:03:07 -0300

    Inclusão arquivos iniciais

---
+++ src/main/kotlin/com/tcc/MsPannotatorMedpipeApplication.kt
@@ -0,0 +1,11 @@
+package com.tcc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MsPannotatorMedpipeApplication
+
+fun main(args: Array<String>) {
+	runApplication<MsPannotatorMedpipeApplication>(*args)
+}
